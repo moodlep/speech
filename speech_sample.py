529d9bf2fbe0cef1c032154a7adee11bd17b0b9b
Perusha Moodley <perusha.moodley@gmail.com>
2017-03-20 09:08:33 +0000

    Initial commit of speech test code from Google Speech API samples page

---
+++ speech_sample.py
@@ -0,0 +1,77 @@
+"""Google Cloud Speech API sample application using the REST API for batch
+processing.
+Example usage: python transcribe.py resources/audio.raw
+"""
+
+# [START import_libraries]
+import argparse
+import base64
+import json
+
+import googleapiclient.discovery
+from oauth2client.client import GoogleCredentials
+# [END import_libraries]
+
+
+# [START authenticating]
+# Application default credentials provided by env variable
+# GOOGLE_APPLICATION_CREDENTIALS
+def get_speech_service():
+    DISCOVERY_URL = 'https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
+    credentials = GoogleCredentials.from_stream('/Users/perusha/git_repos/auth_keys/CloudVisionPrj-3b7fedb71edc.json')
+    return googleapiclient.discovery.build('speech', 'v1beta1', credentials=credentials, discoveryServiceUrl=DISCOVERY_URL)
+# [END authenticating]
+
+
+def main(speech_file):
+    """Transcribe the given audio file.
+    Args:
+        speech_file: the name of the audio file.
+    """
+    # [START construct_request]
+    with open(speech_file, 'rb') as speech:
+        # Base64 encode the binary audio file for inclusion in the JSON
+        # request.
+        speech_content = base64.b64encode(speech.read())
+
+    service = get_speech_service()
+    service_request = service.speech().syncrecognize(
+        body={
+            'config': {
+                # There are a bunch of config options you can specify. See
+                # https://goo.gl/KPZn97 for the full list.
+                'encoding': 'LINEAR16',  # raw 16-bit signed LE samples
+                'sampleRate': 16000,  # 16 khz
+                # See http://g.co/cloud/speech/docs/languages for a list of
+                # supported languages.
+                'languageCode': 'en-US',  # a BCP-47 language tag
+            },
+            'audio': {
+                'content': speech_content.decode('UTF-8')
+                }
+            })
+    # [END construct_request]
+    # [START send_request]
+    response = service_request.execute()
+
+    # First print the raw json response
+    print(json.dumps(response, indent=2))
+
+    # Now print the actual transcriptions
+    for result in response.get('results', []):
+        print('Result:')
+        for alternative in result['alternatives']:
+            print(u'  Alternative: {}'.format(alternative['transcript']))
+    # [END send_request]
+
+
+# [START run_application]
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        'speech_file', help='Full path of audio file to be recognized')
+    args = parser.parse_args()
+    main(args.speech_file)
+    # [END run_application]
